3dbbde16f3ad1272a86ed62e911e3856b14e5107
Prashant Tak <prashantrameshtak@gmail.com>
2020-10-06 13:11:10 +0530

    Add refresh client modes

--- modules/app/torrent/config.el
+++ modules/app/torrent/config.el
@@ -1,1 +1,10 @@
 ;;; app/torrent/config.el -*- lexical-binding: t; -*-
+
+(use-package! transmission
+   ;; :init
+   :config
+   (setq transmission-refresh-modes '(transmission-mode
+                                      transmission-files-mode
+                                      transmission-info-mode
+                                      transmission-peers-mode))
+   )
