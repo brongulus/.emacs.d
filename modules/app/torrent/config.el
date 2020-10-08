2d6aa396b3c020d42e6733dad30c7f905c3c0934
Prashant Tak <prashantrameshtak@gmail.com>
2020-10-08 21:14:47 +0530

    Fix the daemon issue and snipe issue

--- modules/app/torrent/config.el
+++ modules/app/torrent/config.el
@@ -1,19 +1,15 @@
 ;;; app/torrent/config.el -*- lexical-binding: t; -*-
 
 (use-package! transmission
-  ;;:init
-  ;;(make-process
-  ;; :name "Transmission Daemon"
-  ;; :buffer nil
-  ;; :command '("transmission-daemon")
-  ;; )
-  ;;(setq evil-snipe-disabled-modes transmission-mode)
+  :init
+  (after! evil-snipe
+    (add-to-list 'evil-snipe-disabled-modes 'transmission-mode)
+    )
   :config
   (setq transmission-refresh-modes '(transmission-mode
                                      transmission-files-mode
                                      transmission-info-mode
                                      transmission-peers-mode))
-  ;; (map!
-  ;;       :map transmission-mode-map
-  ;;       "s" #'transmission-stats)
+  (map! :map transmission-mode-map
+        :n "s" #'transmission-stats)
   )
