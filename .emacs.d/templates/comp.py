293956642082868a6ddcebfba302d31f3851232c
brongulus <prashantrameshtak@gmail.com>
2023-07-03 00:23:21 +0530

    Oops, forgot to add new files (pkg, py template)

---
+++ .emacs.d/templates/comp.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline()
+def II(): return int(input)
+def LI(): return list(map(int, input.split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def SI(): return input.rstrip()
+
