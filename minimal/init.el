39f913af513560abb33bc121222b138e8bbf1146
brongulus <prashantrameshtak@gmail.com>
2026-02-21 03:05:11 +0530

    minimal: no modeline, nov.el

--- minimal/init.el
+++ minimal/init.el
@@ -1,20 +1,23 @@
-;;; init.el --- 🦬 -*- lexical-binding: t; -*
+;;; init.el - 🦬 ---  -*- lexical-binding: t; -*
 ;; (load "~/.emacs.d/lisp/benchmarking.el" :noerr :no-message)
 ;;; Better defaults ---
-(setq-default auto-save-default nil auto-save-list-file-prefix nil
-              create-lockfiles nil
-              custom-file "/tmp/emacs-custom"
-              cursor-in-non-selected-windows nil
+(if (display-graphic-p) (setq-default mode-line-format (list ""))
+  (setq-default mode-line-format (make-string (window-width) ?─ t)))
+(if (display-graphic-p) (set-face-attribute 'mode-line nil :height 0.1)
+  (dolist (fc '(mode-line mode-line-inactive)) (set-face-attribute fc nil :background 'unspecified)))
+(setq-default auto-save-file-name-transforms `((".*" "~/.emacs.d/backup/" t))
+              backup-directory-alist `(("." . "~/.emacs.d/backup/"))
+              create-lockfiles nil custom-file "/tmp/emacs-custom"
+              warning-minimum-level :error cursor-in-non-selected-windows nil
               frame-resize-pixelwise t make-backup-files nil
               eldoc-echo-area-use-multiline-p nil
               imenu-flatten t inhibit-startup-screen t
               mode-line-collapse-minor-modes '(not flymake-mode)
               mode-line-end-spaces nil mode-line-compact t
               copy-region-blink-delay 0 display-line-numbers-width 4
-              truncate-lines nil tab-bar-show nil
+              truncate-lines nil tab-bar-show nil sentence-end-double-space nil
               ediff-split-window-function 'split-window-horizontally
               ediff-window-setup-function 'ediff-setup-windows-plain
-              warning-minimum-level :error
               use-short-answers t uniquify-buffer-name-style 'forward
               tab-width 4 line-spacing 5 indent-tabs-mode nil
               fill-column 140 text-scale-mode-step 1.3)
@@ -39,7 +42,7 @@
 	  vc-git-diff-switches '("--patch-with-stat" "--histogram")
 	  vc-git-shortlog-switches '("--stat")
       project-vc-extra-root-markers '("Cargo.toml" "build.zig" "go.work")
-      eshell-hist-ignoredups 'erase eshell-history-size 20000
+      eshell-banner-message "\n" eshell-hist-ignoredups 'erase eshell-history-size 20000
       eshell-save-history-on-exit t eshell-glob-case-insensitive t
       grep-command "rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)"
       grep-command-position 27 ido-enable-flex-matching t ido-everywhere t
@@ -59,7 +62,7 @@
 (kill-ring-deindent-mode 1) (save-place-mode 1) (global-visual-line-mode 1) (repeat-mode 1)
 (add-hook 'emacs-startup-hook
           (lambda () (ido-mode 'both) (which-key-mode 1) (global-auto-revert-mode 1)
-            (savehist-mode 1) (viper-mode)))
+            (delete-selection-mode 1) (savehist-mode 1) (viper-mode)))
 ;;; Keys ---
 (setq viper-mode t viper-expert-level 5
       viper-inhibit-startup-message t viper-want-ctl-h-help t
@@ -98,18 +101,17 @@
                                (insert ?j)))))
 (keymap-set vc-prefix-map "f" (lambda () (interactive) (vc-git--pushpull "push" nil '("--force-with-lease"))))
 (keymap-set vc-prefix-map "e" #'vc-ediff)
+(with-eval-after-load 'dired (keymap-set dired-mode-map "SPC" ctl-x-map))
+(with-eval-after-load 'doc-view
+  (keymap-set doc-view-mode-map "SPC" ctl-x-map)
+  (keymap-set doc-view-mode-map "j" #'doc-view-scroll-up-or-next-page)
+  (keymap-set doc-view-mode-map "k" #'doc-view-scroll-down-or-previous-page))
 ;;; Visuals ---
 (dolist (face '(default fixed-pitch variable-pitch))
   (set-face-attribute face nil :font "Input Mono Narrow" :height (if (eq system-type 'android) 160 140)))
 (dolist (face '(vertical-border font-lock-comment-face))
   (set-face-attribute face nil :foreground 'unspecified :inherit '(shadow default)))
 (set-face-attribute 'fringe nil :background 'unspecified)
-(let ((common (list :background 'unspecified :foreground 'unspecified
-                    :inverse-video (not (display-graphic-p))
-                    :box '(:line-width 1 :style flat-button)
-                    :overline (face-foreground 'shadow))))
-  (apply #'set-face-attribute 'mode-line nil :inherit 'default common)
-  (apply #'set-face-attribute 'mode-line-inactive nil :inherit 'shadow common))
 (set-face-attribute 'default nil :foreground "#222323" :background "#eae8e1")
 (add-hook 'post-command-hook
           (lambda () (unless (eq (buffer-modified-p) (bound-and-true-p curs-mod))
@@ -123,39 +125,16 @@
 (dolist (face '(minibuffer-prompt font-lock-keyword-face
                                   font-lock-function-name-face font-lock-type-face))
   (set-face-attribute face nil :foreground 'unspecified :weight 'bold))
-(custom-set-faces '(eglot-highlight-symbol-face ((((background dark))  :background "#2a2c2c")
-                                                 (((background light)) :background "#d9d7d0"))))
+(custom-set-faces '(highlight ((((background dark))  :background "#2a2c2c")
+                               (((background light)) :background "#d9d7d0"))))
+(custom-set-faces '(eglot-highlight-symbol-face ((t :inherit highlight))))
 (custom-set-faces '(link ((((background light)) :foreground "RoyalBlue3" :underline t)
                           (((background dark))  :foreground "#80A0C2" :underline t))))
-(custom-set-faces '(region ((((background light)) :background "lightgoldenrod2" :extend nil)
-                            (((background dark))  :background "#6b6236" :extend nil))))
+(set-face-attribute 'region nil :background "lightgoldenrod2" :foreground "#222323" :extend nil)
 (add-hook 'prog-mode-hook
           (lambda ()
-            (font-lock-add-keywords nil '(("\\<\\(FIXME\\|HACK\\|TODO\\|WIP\\|BUG\\|DONE\\)" 1 'highlight t)
+            (font-lock-add-keywords nil '(("\\<\\(FIXME\\|HACK\\|TODO\\|WIP\\|BUG\\|DONE\\)" 1 'match t)
                                           (";" . 'shadow)))))
-(defvar tab-bar--tab-keymaps
-  (let ((v (make-vector 20 nil)))
-    (dotimes (i 20 v) (let ((m (make-sparse-keymap)))
-                        (define-key m [mode-line mouse-1] `(lambda () (interactive) (tab-bar-select-tab ,(1+ i))))
-                        (aset v i m)))))
-(setq mode-line-front-space
-      '(:eval (when (> (length (tab-bar-tabs)) 1)
-                (propertize
-                 (concat " "
-                         (mapconcat
-                          (lambda (i) (propertize (if (= i (tab-bar--current-tab-index)) "⦿" "○")
-                                                  'mouse-face 'mode-line-highlight
-                                                  'local-map (aref tab-bar--tab-keymaps i)))
-                          (number-sequence 0 (1- (length (tab-bar-tabs)))) " ")
-                         " ")))))
-(setq-default mode-line-format (remove '(project-mode-line project-mode-line-format) mode-line-format))
-(with-eval-after-load 'eglot
-  (setq mode-line-misc-info
-        '((which-function-mode (which-func-mode (which-func--use-mode-line ("" which-func-format " "))))
-          (:eval (when (and (bound-and-true-p eglot--managed-mode) (eglot-managed-p)) eglot-mode-line-progress)))))
-(with-eval-after-load 'viper
-  (let ((cell (memq 'mode-line-modes mode-line-format)))
-    (setcar cell 'viper-mode-string) (setcdr cell (cons 'mode-line-format-right-align (cons 'mode-line-modes (cdr cell))))))
 ;;; Programming stuff ---
 (dolist (fn '(hs-minor-mode which-function-mode display-line-numbers-mode electric-pair-local-mode
                             completion-preview-mode goto-address-mode))
@@ -187,23 +166,31 @@
 (add-hook #'eshell-mode-hook 
           (lambda nil (define-key eshell-hist-mode-map (kbd "C-r") #'eshell-insert-history)))
 ;;; Miscellaneous ---
+(setq shr-max-image-proportion 0.5)
+(defun my-shr-tag-render (tag face-spec)
+  (let ((default-renderer (intern (format "shr-tag-%s" tag))))
+    (lambda (dom) (let ((start (point)))
+                    (funcall default-renderer dom) (add-face-text-property start (point) face-spec)))))
+(with-eval-after-load 'shr
+  (setq shr-external-rendering-functions
+        `((pre        . ,(my-shr-tag-render 'pre        '(:inherit highlight :extend t)))
+          (blockquote . ,(my-shr-tag-render 'blockquote '(:slant italic)))
+          (h1         . ,(my-shr-tag-render 'h1         '(:inherit bold :height 1.3)))
+          (h2         . ,(my-shr-tag-render 'h2         '(:inherit bold :height 1.2)))
+          (h3         . ,(my-shr-tag-render 'h3         '(:inherit bold :height 1.2))))))
 (with-eval-after-load 'eww
   (add-hook 'eww-after-render-hook #'viper-mode)
   (setq eww-header-line-format nil)
   (setq eww-auto-rename-buffer 'title))
-(with-eval-after-load 'doc-view
-  (define-key doc-view-mode-map (kbd "SPC") ctl-x-map)
-  (define-key doc-view-mode-map (kbd "j") #'doc-view-scroll-up-or-next-page)
-  (define-key doc-view-mode-map (kbd "k") #'doc-view-scroll-down-or-previous-page))
 (setopt minibuffer-completion-auto-choose t completion-ignore-case t
         completions-format 'one-column completions-header-format nil
-        completion-show-help nil completions-max-height 13
-        completion-preview-minimum-symbol-length 2
+        completion-show-help nil completions-max-height 11
+        completion-preview-minimum-symbol-length 2 completion-auto-select 'second-tab
         completions-sort 'historical completions-detailed t
         completion-eager-update t completion-auto-help 'visible
-        completion-styles '(partial-completion basic flex))
-(define-key minibuffer-local-completion-map [remap previous-line] #'minibuffer-previous-completion)
-(define-key minibuffer-local-completion-map [remap next-line]     #'minibuffer-next-completion)
+        completion-styles '(initials partial-completion basic flex))
+(keymap-set minibuffer-local-completion-map "C-r" #'minibuffer-previous-completion)
+(keymap-set minibuffer-local-completion-map "C-s" #'minibuffer-next-completion)
 (add-to-list 'display-buffer-alist
              '("\\*\\(Completions\\|xref\\|Occur.*\\|eldoc\\|vc.*\\|compilation.*\\|Flymake.*\\)\\*"
                (display-buffer-in-side-window) (side . bottom) (window-height . 0.30)
@@ -283,3 +270,64 @@
        (forward-line arg))
 (defun del-vi nil (interactive)
        (if (use-region-p) (call-interactively 'kill-region) (delete-char 1)))
+;;; Nov.el alternative (claude)
+(defun epub--xml (path)
+  (with-temp-buffer (insert-file-contents path) (libxml-parse-xml-region (point-min) (point-max))))
+(defun epub-open (&optional file) (interactive "fEPUB: ")
+       (require 'shr)
+       (let* ((file (or file buffer-file-name))
+              (dir (make-temp-file "epub-" t))
+              (_ (call-process "unzip" nil nil nil "-qq" "-od" dir file))
+              (opf-path (dom-attr (car (dom-by-tag (epub--xml (expand-file-name "META-INF/container.xml" dir)) 'rootfile)) 'full-path))
+              (opf-dir (file-name-directory (expand-file-name opf-path dir)))
+              (opf (epub--xml (expand-file-name opf-path dir)))
+              (manifest (mapcar (lambda (i) (cons (dom-attr i 'id) (expand-file-name (url-unhex-string (dom-attr i 'href)) opf-dir)))
+                                (dom-by-tag opf 'item))))
+         (let ((inhibit-read-only t)) (erase-buffer)
+              (dolist (f (delq nil (mapcar (lambda (r) (cdr (assoc (dom-attr r 'idref) manifest))) (dom-by-tag opf 'itemref))))
+                (when (string-match-p "\\.x?html?\\'" f)
+                  (shr-insert-document
+                   (with-temp-buffer (insert-file-contents f)
+                                     (while (re-search-forward "\\(src\\|href\\)=\"\\([^\"]+\\)\"" nil t)
+                                       (let ((v (match-string 2)))
+                                         (unless (string-match-p "^[a-z]+://" v)
+                                           (replace-match
+                                            (format "%s=\"file://%s\"" (match-string 1)
+                                                    (expand-file-name (url-unhex-string v) (file-name-directory f))) t t))))
+                                     (libxml-parse-html-region (point-min) (point-max)))))))
+         (goto-char (point-min)) (eww-mode) (viper-mode)
+         (setq buffer-file-name file default-directory (file-name-directory file))
+         (setq-local revert-buffer-function #'ignore write-contents-functions '(ignore))
+         (add-hook 'after-change-functions (lambda (&rest _) (set-buffer-modified-p nil) (delete-directory dir t)) nil t)))
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . epub-open))
+;;; Archive (trying no mode-line)
+;; (let ((common (list :background 'unspecified :foreground 'unspecified
+;;                     :inverse-video (not (display-graphic-p))
+;;                     :height (if (eq system-type 'android) 160 140)
+;;                     :box '(:line-width 1 :style flat-button)
+;;                     :overline (face-foreground 'shadow))))
+;;   (apply #'set-face-attribute 'mode-line nil :inherit 'default common)
+;;   (apply #'set-face-attribute 'mode-line-inactive nil :inherit 'shadow common))
+;; (defvar tab-bar--tab-keymaps
+;;   (let ((v (make-vector 20 nil)))
+;;     (dotimes (i 20 v) (let ((m (make-sparse-keymap)))
+;;                         (define-key m [mode-line mouse-1] `(lambda () (interactive) (tab-bar-select-tab ,(1+ i))))
+;;                         (aset v i m)))))
+;; (setq mode-line-front-space
+;;       '(:eval (when (> (length (tab-bar-tabs)) 1)
+;;                 (propertize
+;;                  (concat " "
+;;                          (mapconcat
+;;                           (lambda (i) (propertize (if (= i (tab-bar--current-tab-index)) "⦿" "○")
+;;                                                   'mouse-face 'mode-line-highlight
+;;                                                   'local-map (aref tab-bar--tab-keymaps i)))
+;;                           (number-sequence 0 (1- (length (tab-bar-tabs)))) " ")
+;;                          " ")))))
+;; (setq-default mode-line-format (remove '(project-mode-line project-mode-line-format) mode-line-format))
+;; (with-eval-after-load 'eglot
+;;   (setq mode-line-misc-info
+;;         '((which-function-mode (which-func-mode (which-func--use-mode-line ("" which-func-format " "))))
+;;           (:eval (when (and (bound-and-true-p eglot--managed-mode) (eglot-managed-p)) eglot-mode-line-progress)))))
+;; (with-eval-after-load 'viper (setq global-mode-string nil)
+;;                       (let ((cell (memq 'mode-line-modes mode-line-format)))
+;;                         (setcar cell 'viper-mode-string) (setcdr cell (cons 'mode-line-format-right-align (cons 'mode-line-modes (cdr cell))))))
